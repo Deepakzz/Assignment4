fae93f0969743488648b1754e18c5b3fdbbd0690
Deepak Rameshkumar <dr439@afsconnect2.njit.edu>
2017-02-26 21:06:41 -0500

    Add code category list

--- category_list.php
+++ category_list.php
@@ -28,7 +28,15 @@ $statement->closeCursor();
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>
-        
+        <?php foreach ($categories as $category) : ?>
+		     <tr>
+		     <td><?php echo $category['categoryName']; ?></td>
+	             <td><form action="delete_category.php" method="post">
+		     <input type="hidden" name="categoryID" value="<?php echo $category['categoryID']; ?>">
+		     <input type="submit" value="Delete">
+		     </form></td>
+		    </tr>
+	        	<?php endforeach;?>
         <!-- add code for the rest of the table here -->
     
     </table>
